4994851c8f32051208b76bd0cafbd616be531cde
Evgeny Simonov <jdsimonov2@gmail.com>
2025-09-06 13:17:33 +0300

    new file:   images/arrow-long-left.svg 	new file:   images/arrow-long-right.svg 	new file:   images/turkey.jpg 	modified:   index.html 	new file:   scripts/data.js 	new file:   scripts/index.js 	modified:   styles.css

---
+++ scripts/data.js
@@ -0,0 +1,9 @@
+const dataForCourses = [{
+    image: './images/turkey.jpg',
+    location: 'ТУРЦИЯ',
+    price: 'ОТ € 1 800 ЗА ЧЕЛОВЕКА'
+},{
+    image: './images/turkey.jpg',
+    location: 'ИСПАНИЯ',
+    price: 'ОТ € 2 600'
+}]
