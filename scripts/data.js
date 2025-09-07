1145ac63d7d004a44138168527044c67a19f99f7
Evgeny Simonov <jdsimonov2@gmail.com>
2025-09-07 14:45:31 +0300

    modified:   index.html 	modified:   scripts/data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,9 +1,74 @@
 const dataForCourses = [{
-    image: './images/turkey.jpg',
+    image: '../images/turkey.jpg',
     location: 'ТУРЦИЯ',
     price: 'ОТ € 1 800 ЗА ЧЕЛОВЕКА'
 },{
-    image: './images/turkey.jpg',
+    image: '../images/turkey.jpg',
     location: 'ИСПАНИЯ',
     price: 'ОТ € 2 600'
 }]
+const dataForGallery = [
+    {
+        image:'../images/galery/2025-08-28 15.43.41.jpg',
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_03.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_09.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_20.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_24.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_30.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_33.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_36.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_41.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_44.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_54.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_45_58.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_00.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_03.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_08.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_17.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_27.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_32.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_39.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_48.jpg'
+    },
+    {
+        image:'../images/galery/IMAGE 2025-08-28 15_46_52.jpg'
+    }
+]
